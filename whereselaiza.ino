1bf1b3ccb2450bd46a11be7a3413fb4dfe1a6e6c
Marco A. Longoria <marcolongoria@Marcos-MacBook-Pro.local>
2017-03-01 18:38:23 -0600

    Add SIM808 AT commands test/Adafruit Test and Updated whereselaiza.ino

--- whereselaiza.ino
+++ whereselaiza.ino
@@ -1,17 +1,27 @@
-*/ WheresElaiza - IoT Dog Tracker 
+/*
+	WheresElaiza
+
+		SIM 808 based GSM/GPS Dog Tracker 
+	
 	By Marco Longoria 
 
 */
 
+#include "Adafruit_FONA.h"
+#include <SoftwareSerial.h>
 
+#define 
+char replybuffer[255];
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+//	HardwareSerial *fonaSerial = &Serial1; 
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
-void setup();
-
+void setup(){
+	while (!Serial);
 	Serial.begin(115200);
+	Serial.println(F("WheresElaiza!"));
+	Serial.println(F("Initializing SIM808..."));
 
-
-
-void loop();
-
-	Serial.print("Hello World my name is Elaiza");
-	
+	fonaSerial->begin
+}
